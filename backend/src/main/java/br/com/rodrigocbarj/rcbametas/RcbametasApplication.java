c07e57ada55c60f7f4bcb468f86564395c75b69f
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-09-06 19:41:30 -0300

    create Spring Boot project

---
+++ backend/src/main/java/br/com/rodrigocbarj/rcbametas/RcbametasApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigocbarj.rcbametas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcbametasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcbametasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/rodrigocbarj/rcbametas/RcbametasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigocbarj.rcbametas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcbametasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
